f1ace7513c10cc4d8a98a43830f311ac0a48777f
MattLytv <121431435+MattLytv@users.noreply.github.com>
2022-12-26 11:45:24 +0100

    Add files via upload

---
+++ task 1.cs
@@ -0,0 +1,107 @@
+﻿
+using System.Security.AccessControl;
+
+/*Даний інтерфейс поганий тим, що він включає занадто багато методів.
+ А що, якщо наш клас товарів не може мати знижок або промокодом, або для нього немає сенсу встановлювати матеріал з 
+ якого зроблений (наприклад, для книг). Таким чином, щоб не реалізовувати в кожному класі невикористовувані в ньому методи, краще 
+розбити інтерфейс на кілька дрібних і кожним конкретним класом реалізовувати потрібні інтерфейси.
+Перепишіть, розбивши інтерфейс на декілька інтерфейсів, керуючись принципом розділення інтерфейсу. 
+Опишіть класи книжки (розмір та колір не потрібні, але потрібна ціна та знижки) та верхній одяг (колір, розмір, ціна знижка),
+які реалізують притаманні їм інтерфейси.*/
+
+interface IDiscount
+{
+    void ApplyDiscount(string discount);
+}
+
+interface IPromocode
+{
+    void ApplyPromocode(string promocode);
+}
+
+interface ISizeAndColor
+{
+    void SetColor(string color);
+    void SetSize(string size);
+}
+
+interface IPrice
+{
+    void SetPrice(double price);
+}
+
+class Book : IDiscount, IPrice
+{
+    public Book(string name)
+    {
+        this.name = name;
+    }
+    private string name { get; set; }
+    private double price { get; set; }
+    private string discount { get; set; }
+    public void ApplyDiscount(string discount)
+    {
+        this.discount = discount;
+    }
+    public void SetPrice(double price)
+    {
+        this.price = price;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine("\nName - " + name + "\nDiscount - " + discount + "\nPrice - " + price);
+    }
+}
+
+class OutWear : IDiscount, ISizeAndColor, IPrice
+{
+    private string name { get; set; }
+    private double price { get; set; }
+    private string discount { get; set; }
+    private string size { get; set; }
+    private string color { get; set; }
+    public OutWear(string name)
+    {
+        this.name = name;
+    }
+    public void ApplyDiscount(string discount)
+    {
+        this.discount = discount;
+    }
+    public void SetPrice(double price)
+    {
+        this.price = price;
+    }
+    public void SetColor(string color)
+    {
+        this.color = color;
+    }
+    public void SetSize(string size)
+    {
+        this.size = size;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine("\nName - " + name + "\nDiscount - " + discount + "\nPrice - " + price + "\nSize - " + size + "\nColor - " + color);
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new Book("book1");
+        book1.ApplyDiscount("20%");
+        book1.SetPrice(120);
+        book1.ShowInfo();
+
+        OutWear outwear1 = new OutWear("outwear1");
+        outwear1.ApplyDiscount("0%");
+        outwear1.SetPrice(3400);
+        outwear1.SetSize("M");
+        outwear1.SetColor("red");
+        outwear1.ShowInfo();
+
+        Console.ReadKey();
+    }
+}
---
+++ task 4.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+namespace Decorator.Examples
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Create ConcreteComponent and two Decorators
+            ConcreteChristmasTree xTree = new ConcreteChristmasTree();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+            // Link decorators
+            d1.SetComponent(xTree);
+            d2.SetComponent(d1);
+
+            d2.Operation();
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+    // "Component"
+    abstract class ChristmasTree
+    {
+        public abstract void Operation();
+    }
+    // "ConcreteComponent"
+    class ConcreteChristmasTree : ChristmasTree
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Новорічна ялинка без прикрас");
+        }
+    }
+    // "Decorator"
+    abstract class Decorator : ChristmasTree
+    {
+        protected ChristmasTree component;
+
+        public void SetComponent(ChristmasTree component)
+        {
+            this.component = component;
+        }
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+    // "ConcreteDecoratorA"
+    class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Додано новорічні прикраси!");
+        }
+    }
+
+    // "ConcreteDecoratorB"
+    class ConcreteDecoratorB : Decorator
+    {
+
+        private string color1 = "жовтим";
+        private string color2 = "червоним";
+        private string color3 = "зеленим";
+        private string color4 = "синім";
+        private string color5 = "рожевим";
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Додано гірлянди!");
+            AddedBehavior();
+        }
+        void AddedBehavior()
+        {
+            var rand = new Random();
+            switch (rand.Next(1, 6))
+            {
+                case 1:
+                    Console.WriteLine("Гірлянда сяє" + " {0} " + "кольором", color1);
+                    break;
+                case 2:
+                    Console.WriteLine("Гірлянда сяє" + " {0} " + "кольором", color2);
+                    break;
+                case 3:
+                    Console.WriteLine("Гірлянда сяє" + " {0} " + "кольором", color3);
+                    break;
+                case 4:
+                    Console.WriteLine("Гірлянда сяє" + " {0} " + "кольором", color4);
+                    break;
+                case 5:
+                    Console.WriteLine("Гірлянда сяє" + " {0} " + "кольором", color5);
+                    break;
+            }
+        }
+    }
+}
